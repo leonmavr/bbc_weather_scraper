ef50e42115d857027a17972784ae9fcc562950eb
leonmavr <0xleo.git@gmail.com>
2025-01-03 02:34:30 +0200

    hourly report: cache the request in a file

--- hourly.py
+++ hourly.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import os
+import time
 from collections import namedtuple, defaultdict
 from datetime import datetime, timedelta
+from typing import Dict
 
 WeatherReport = namedtuple(
     "WeatherReport",
@@ -16,46 +19,67 @@ WeatherReport = namedtuple(
     ]
 )
 
+CACHE_FILE = "/tmp/weather_hourly_2925533.json"
+CACHE_TTL_SEC = 3600
 
-def request_hourly(url='https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/2925533') -> WeatherReport:
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+
+def request_weather(url) -> Dict:
+    '''
+    Requests data from BBC's API. If already requested up to `CACHE_TTL_SEC` ago,
+    read from a cached file.
+    '''
+    if os.path.exists(CACHE_FILE):
+        file_age = time.time() - os.path.getmtime(CACHE_FILE)
+        if file_age < CACHE_TTL_SEC:
+            with open(CACHE_FILE, 'r') as f:
+                return json.load(f)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        forecasts = data.get("forecasts", [])
-        weather_data = defaultdict(list)
-
-        for iday, forecast in enumerate(forecasts):
-            # hourly weather reports
-            reports = forecast.get("detailed", {}).get("reports", [])
-            for ihour, report in enumerate(reports):
-                try:
-                    # you can fetch more metrics here
-                    localDate = report["localDate"]
-                    timeslot = report["timeslot"]
-                    weatherTypeText = report["weatherTypeText"]
-                    temperatureC = report["temperatureC"]
-                    precipitationProbabilityInPercent = report["precipitationProbabilityInPercent"]
-                    humidity = report["humidity"]
-                    windSpeedKph = report["windSpeedKph"]
-
-                    weather_report = WeatherReport(
-                        localDate=localDate,
-                        timeslot=timeslot,
-                        weatherTypeText=weatherTypeText,
-                        temperatureC=temperatureC,
-                        precipitationProbabilityInPercent=precipitationProbabilityInPercent,
-                        humidity=humidity,
-                        windSpeedKph=windSpeedKph
-                    )
-                    weather_data[localDate].append(weather_report)
-                except KeyError:
-                    pass
+        with open(CACHE_FILE, 'w') as f:
+            json.dump(data, f)
+        return data
     else:
         print(f"Failed to fetch data. HTTP status code: {response.status_code}")
+        return None
+
+
+def request_hourly(url='https://weather-broker-cdn.api.bbci.co.uk/en/forecast/aggregated/2925533') -> WeatherReport:
+    data = request_weather(url)
+    if not data:
+        return defaultdict(list)
+
+    forecasts = data.get("forecasts", [])
+    weather_data = defaultdict(list)
+
+    for iday, forecast in enumerate(forecasts):
+        reports = forecast.get("detailed", {}).get("reports", [])
+        for ihour, report in enumerate(reports):
+            try:
+                # you can inspect the API yourself and grab more data here
+                localDate = report["localDate"]
+                timeslot = report["timeslot"]
+                weatherTypeText = report["weatherTypeText"]
+                temperatureC = report["temperatureC"]
+                precipitationProbabilityInPercent = report["precipitationProbabilityInPercent"]
+                humidity = report["humidity"]
+                windSpeedKph = report["windSpeedKph"]
+
+                weather_report = WeatherReport(
+                    localDate=localDate,
+                    timeslot=timeslot,
+                    weatherTypeText=weatherTypeText,
+                    temperatureC=temperatureC,
+                    precipitationProbabilityInPercent=precipitationProbabilityInPercent,
+                    humidity=humidity,
+                    windSpeedKph=windSpeedKph
+                )
+                weather_data[localDate].append(weather_report)
+            except KeyError:
+                pass
     return weather_data
+
 
 def draw_bar(from_, to_, value, bar_width=20) -> str:
     block = '\u2588'
@@ -68,6 +92,7 @@ def draw_bar(from_, to_, value, bar_width=20) -> str:
     bar = border + block * blocks_to_fill + ' ' * (bar_width - 2 - blocks_to_fill) + border
     return bar
 
+
 def fmt_weather_data(data, from_, to_) -> str:
     hour = data.timeslot
     temp = data.temperatureC
@@ -75,18 +100,19 @@ def fmt_weather_data(data, from_, to_) -> str:
     humid = data.humidity
     wspeed = data.windSpeedKph
     text = data.weatherTypeText
-    ret =  f"{hour} \t {temp_bar} \t {temp} °C \t {humid} % \t {wspeed} kph \t {text}"
+    ret = f"{hour} \t {temp_bar} \t {temp} °C \t {humid} % \t {wspeed} kph \t {text}"
     return ret
+
 
 def fmt_day_hourly(days_from_now=0) -> str:
     weather_data = request_hourly()
-
     target_date = (datetime.now() + timedelta(days=days_from_now)).strftime('%Y-%m-%d')
     ret = ''
 
     if target_date in weather_data:
         temp_min, temp_max = -30, 45
         for report in weather_data[target_date]:
+            # grab the min and mxx temperature to adjust the steps in the bar
             if report.temperatureC > temp_min:
                 temp_min = report.temperatureC
             if report.temperatureC < temp_max:
@@ -94,10 +120,4 @@ def fmt_day_hourly(days_from_now=0) -> str:
         temp_min, temp_max = temp_max, temp_min
         for report in weather_data[target_date]:
             ret += fmt_weather_data(report, temp_min, temp_max) + '\n'
-    else:
-        print(f"No weather data available for {target_date}.")
     return ret
-
-if __name__ == '__main__':
-    for i in range(14):
-        print(fmt_day_hourly(i))
